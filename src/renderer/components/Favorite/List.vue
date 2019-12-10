be4df75e445493b4c7be0e74f32a4f2ad4e01369
y2468101216 <y2468101216@gmail.com>
2019-12-10 11:33:43 +0800

    add favorite price

--- src/renderer/components/Favorite/List.vue
+++ src/renderer/components/Favorite/List.vue
@@ -6,6 +6,9 @@
         <template v-slot:cell(name)="data">
             {{ data.item.data.name }}
         </template>
+        <template v-slot:cell(price)="data">
+            {{ prices[data.item.code] ? prices[data.item.code] : 'no data'}}
+        </template>
         <template v-slot:cell(action)="data">
             <b-button v-on:click="redirect('price', data.item.code)">收盤走勢圖</b-button>
             <b-button v-on:click="redirect('minute-price', data.item.code)">即時走勢圖</b-button>
@@ -22,8 +25,10 @@
         fields: [
           { key: 'code', label: '股票代碼' },
           { key: 'name', label: '股票名稱' },
+          { key: 'price', label: '股票當前成交價(不會即時更新)' },
           { key: 'action', label: '動作' }
-        ]
+        ],
+        prices: {}
       }
     },
     mounted () {
@@ -33,11 +38,34 @@
       renderFavorites () {
         let that = this
         this.$db.favorites.toArray((favorites) => {
-          that.favorites = favorites
+          let arr = []
+          favorites.forEach(function (element) {
+            element.price = 'no data'
+            arr.push(element)
+            that.getPrice(element)
+          })
+          that.favorites = arr
         })
       },
       redirect (action, code) {
         this.$router.push({name: action, params: {favoriteStockId: code}})
+      },
+      getPrice (element) {
+        let bodyFrom = new FormData()
+        bodyFrom.set('dataset', 'TaiwanStockPriceMinute')
+        bodyFrom.set('stock_id', element.code)
+        bodyFrom.set('date', this.$moment().format('YYYY-MM-DD'))
+        let that = this
+        this.$http.post(this.$base_url,
+          bodyFrom, {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+          }).then(function (response) {
+          let price = response.data.data.deal_price.pop()
+          that.$set(that.prices, element.code, price)
+          that.$forceUpdate()
+        })
       }
     }
   }
